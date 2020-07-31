969fb588f8fe213ca316ff0c0be39bae06be1c26
Bruno Fuentes (brunidas) <bruno.e.fuentes97@gmail.com>
2020-07-30 23:30:57 -0300

    DELETED: carpeta fundacion

--- fundacion/routes/involucrate.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const involucrateController = require('../controllers/involucrateController')
-
-router.get('/formulario', involucrateController.formulario);
-router.post('/formulario', involucrateController.enviarFormulario);
-
-
-module.exports = router;
